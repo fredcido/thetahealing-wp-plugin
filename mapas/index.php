fba085253feee8eedb9045deff24f3a32ae9fb3d
Frederico Estrela <f.estrelagaiva@elsevier.com>
2019-02-15 11:48:59 +0100

    Fix mobile

--- mapas/index.php
+++ mapas/index.php
@@ -3,8 +3,8 @@
 		<p>
 			<strong>Selecione um estado</strong>
 			<select id="uf-select" name="uf-select">
+				<option value=""></option>
 				<?php foreach ($ufs as $uf => $label) : ?>
-					<option value=""></option>
 					<option value="<?php echo $uf ;?>"><?php echo $uf; ?></option>
 				<?php endforeach; ?>
 			</select>
